5aad6142a78910d3e65ef8cfa26b5c665a8f8542
Rhino875 <shaldon.neyschens@gmail.com>
2022-10-21 11:37:41 +0200

    Home Page Complete for PC

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,20 @@
+<script lang="ts">
+    import './header.scss';
+</script>
+
 <div class="main_header">
-    <ul>
-        <li><a href="/">HOME</a></li>
-        <li><a href="/products_and_services">PRODUCTS AND SERVICES</a></li>
-        <li><a href="/visual_cvs">VISUAL CV'S</a></li>
-        <li><a href="/work_with_us">WORK WITH US</a></li>
-        <li><a href="/about_us">ABOUT US</a></li>
-        <li><a href="/contact_us">CONTACT US</a></li>
-    </ul>
+    <div class="header_left">
+        <ul>
+            <li><a href="/">HOME</a></li>
+            <li><a href="/products_and_services">PRODUCTS AND SERVICES</a></li>
+            <li><a href="/visual_cvs">VISUAL CV'S</a></li>
+            <li><a href="/work_with_us">WORK WITH US</a></li>
+            <li><a href="/about_us">ABOUT US</a></li>
+            <li><a href="/contact_us">CONTACT US</a></li>
+        </ul>
+    </div>
+    <div class="header_right">
+        <a href="/">LOGIN |REGISTER</a>
+    </div>
+
 </div>
