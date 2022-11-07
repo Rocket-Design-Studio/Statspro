cb0da37780b90a2f66b5861e96f9d62e0724e50e
Rhino875 <shaldon.neyschens@gmail.com>
2022-11-07 11:53:48 +0200

    Tablet Fix

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,5 +1,6 @@
 <script>
     import './Header.scss';
+    import '../../../routes/scss/app.scss';
     
         let showMenu = false;
     
@@ -12,14 +13,14 @@
         <div class="main_header">
         <div>
           <nav
-            class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center"
+            class="container px-6 py-8 mx-auto xl:flex xl:justify-between xl:items-center"
           >
             <div class="flex items-center justify-between">
               <button class="border-orange-bright border-2 hover:bg-orange-bright text-white  py-2 px-6 rounded-full">
                 Login/Register
               </button>
               <!-- Mobile menu button -->
-              <div on:click={toggleNavbar} class="flex md:hidden">
+              <div on:click={toggleNavbar} class="flex xl:hidden">
                 <button
                   type="button"
                   class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
@@ -33,16 +34,16 @@
       
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
             <div
-              class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 {showMenu
+              class="flex-col mt-8 space-y-4 xl:flex xl:space-y-0 xl:flex-row xl:items-center xl:space-x-10 xl:mt-0 {showMenu
                 ? 'flex'
                 : 'hidden'}"
             >
-              <a class="text-gray-800 hover:text-blue-400 text-lg hover:text-orange-bright" href="/">HOME</a>
-              <a class="text-gray-800 hover:text-blue-400 text-lg hover:text-orange-bright" href="/products_and_services">PRODUCTS AND SERVICES</a>
-              <a class="text-gray-800 hover:text-blue-400 text-lg hover:text-orange-bright" href="/visual_cvs">VISUAL CV'S</a>
-              <a class="text-gray-800 hover:text-blue-400 text-lg hover:text-orange-bright" href="/contact_us">CONTACT US</a>
-              <a class="text-gray-800 hover:text-blue-400 text-lg hover:text-orange-bright" href="/work_with_us">WORK WITH US</a>
-              <a class="text-gray-800 hover:text-blue-400 text-lg hover:text-orange-bright" href="/about_us">ABOUT US</a>
+              <a class="text-gray-800 hover:text-blue-400 text-base  hover:text-orange-bright" href="/">HOME</a>
+              <a class="text-gray-800 hover:text-blue-400 text-base  hover:text-orange-bright" href="/products_and_services">PRODUCTS AND SERVICES</a>
+              <a class="text-gray-800 hover:text-blue-400 text-base  hover:text-orange-bright" href="/visual_cvs">VISUAL CV'S</a>
+              <a class="text-gray-800 hover:text-blue-400 text-base  hover:text-orange-bright" href="/contact_us">CONTACT US</a>
+              <a class="text-gray-800 hover:text-blue-400 text-base  hover:text-orange-bright" href="/work_with_us">WORK WITH US</a>
+              <a class="text-gray-800 hover:text-blue-400 text-base  hover:text-orange-bright" href="/about_us">ABOUT US</a>
             </div>
           </nav>
         </div>
